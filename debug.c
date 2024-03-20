2e1ee2bf65c0682c8d2c4702f0cfdaa7d8269ab7
Nurdaulet (Nurda) Taumergenov <ntaumergenov@polymercapital.com>
2024-03-20 15:51:05 +0800

    add: init VM

--- debug.c
+++ debug.c
@@ -14,6 +14,16 @@ void disassembleChunk(Chunk* chunk, const char* name) {
     }
 }
 
+static int longConstantInstruction(const char* name, Chunk* chunk, int offset) {
+    uint32_t constant = chunk->code[offset + 1] |
+                        (chunk->code[offset + 2] << 8 |
+                        chunk->code[offset + 3] << 16);
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 4;
+}
+
 static int constantInstruction(const char* name, Chunk* chunk, int offset) {
     // address to constant operand in our chunk->constants (i.e. order is opcode + constant operand)
     uint8_t constant = chunk->code[offset+1];
@@ -31,14 +41,16 @@ static int simpleInstruction(const char* name, int offset) {
 
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
-    if (offset > 0 &&
-        chunk->lines[offset] == chunk->lines[offset - 1]) {
+    int line = getLine(chunk, offset);
+    if (offset > 0 && line == getLine(chunk, offset - 1)) {
         printf("   | ");
     } else {
-        printf("%4d ", chunk->lines[offset]);
+        printf("%4d ", line);
     }
     uint8_t instruction = chunk->code[offset];
     switch (instruction) {
+        case OP_CONSTANT_LONG:
+            return longConstantInstruction("OP_CONSTANT_LONG", chunk, offset);
         case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
         case OP_RETURN:
--- main.c
+++ main.c
@@ -1,18 +1,32 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
+    /*
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
+    */
+
+    writeConstant(&chunk, 1.2, 123);
 
     writeChunk(&chunk, OP_RETURN, 123);
 
+    for (int i = 0; i < 260; i++) {
+      writeConstant(&chunk, 1.2, 123);
+    }
+
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
+
 }
