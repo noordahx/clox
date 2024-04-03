73af2ae838096a688c94eed0090cc6ebbdec6bed
Nurdaulet (Nurda) Taumergenov <ntaumergenov@polymercapital.com>
2024-04-03 18:00:25 +0800

    add: string memory free

--- memory.h
+++ memory.h
@@ -6,12 +6,14 @@
 #define CLOX_MEMORY_H
 
 #include "common.h"
-
+#include "object.h"
 #define ALLOCATE(type, count) \
     (type*)reallocate(NULL, 0, sizeof(type) * (count))
 
 #define GROW_CAPACITY(capacity) \
     ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
 
 #define GROW_ARRAY(type, pointer, oldCount, newCount) \
     (type*)reallocate(pointer, sizeof(type) * (oldCount), \
@@ -21,5 +23,6 @@
     reallocate(pointer, sizeof(type) * (oldCount), 0)
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void freeObjects();
 
 #endif //CLOX_MEMORY_H
