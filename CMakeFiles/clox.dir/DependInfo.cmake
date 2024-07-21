43544429edc3eb5bac4a60dcb37e4d26f2188ef6
nurda@desktop <taumergenov@duck.com>
2024-07-22 00:42:48 +0800

    some changes

---
+++ CMakeFiles/clox.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nurda/fun/clox/chunk.c" "CMakeFiles/clox.dir/chunk.c.o" "gcc" "CMakeFiles/clox.dir/chunk.c.o.d"
+  "/home/nurda/fun/clox/compiler.c" "CMakeFiles/clox.dir/compiler.c.o" "gcc" "CMakeFiles/clox.dir/compiler.c.o.d"
+  "/home/nurda/fun/clox/debug.c" "CMakeFiles/clox.dir/debug.c.o" "gcc" "CMakeFiles/clox.dir/debug.c.o.d"
+  "/home/nurda/fun/clox/main.c" "CMakeFiles/clox.dir/main.c.o" "gcc" "CMakeFiles/clox.dir/main.c.o.d"
+  "/home/nurda/fun/clox/memory.c" "CMakeFiles/clox.dir/memory.c.o" "gcc" "CMakeFiles/clox.dir/memory.c.o.d"
+  "/home/nurda/fun/clox/object.c" "CMakeFiles/clox.dir/object.c.o" "gcc" "CMakeFiles/clox.dir/object.c.o.d"
+  "/home/nurda/fun/clox/scanner.c" "CMakeFiles/clox.dir/scanner.c.o" "gcc" "CMakeFiles/clox.dir/scanner.c.o.d"
+  "/home/nurda/fun/clox/table.c" "CMakeFiles/clox.dir/table.c.o" "gcc" "CMakeFiles/clox.dir/table.c.o.d"
+  "/home/nurda/fun/clox/value.c" "CMakeFiles/clox.dir/value.c.o" "gcc" "CMakeFiles/clox.dir/value.c.o.d"
+  "/home/nurda/fun/clox/vm.c" "CMakeFiles/clox.dir/vm.c.o" "gcc" "CMakeFiles/clox.dir/vm.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
